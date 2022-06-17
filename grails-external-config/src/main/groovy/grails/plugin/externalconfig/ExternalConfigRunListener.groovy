9fc73e167b62f7febb5252605f43a2c95a196faa
Joshua B <joshdev@9ci.com>
2022-06-17 17:05:19 -0600

    fix assemble

--- grails-external-config/src/main/groovy/grails/plugin/externalconfig/ExternalConfigRunListener.groovy
+++ grails-external-config/src/main/groovy/grails/plugin/externalconfig/ExternalConfigRunListener.groovy
@@ -9,8 +9,8 @@ import java.nio.file.Files
 import java.nio.file.Path
 
 import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
 
-import org.grails.config.NavigableMapPropertySource
 import org.grails.config.PropertySourcesConfig
 import org.grails.config.yaml.YamlPropertySourceLoader
 import org.slf4j.Logger
@@ -18,23 +18,27 @@ import org.slf4j.LoggerFactory
 import org.springframework.boot.ConfigurableBootstrapContext
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.SpringApplicationRunListener
+import org.springframework.boot.env.PropertiesPropertySourceLoader
 import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.core.env.ConfigurableEnvironment
 import org.springframework.core.env.MapPropertySource
+import org.springframework.core.env.PropertySource
 import org.springframework.core.io.DefaultResourceLoader
 import org.springframework.core.io.Resource
 import org.springframework.core.io.ResourceLoader
 
 import grails.util.Environment
 
-@SuppressWarnings(['LoggerWithWrongModifiers', 'ReturnsNullInsteadOfEmptyCollection', 'ReturnNullFromCatchBlock',
+@SuppressWarnings(['ReturnsNullInsteadOfEmptyCollection', 'ReturnNullFromCatchBlock',
     'ReturnsNullInsteadOfEmptyCollection', 'EmptyMethod'])
 @CompileStatic
+@Slf4j(category = 'grails.plugin.externalconfig.ExternalConfig')
 class ExternalConfigRunListener implements SpringApplicationRunListener {
 
     private ResourceLoader defaultResourceLoader = new DefaultResourceLoader()
     private YamlPropertySourceLoader yamlPropertySourceLoader = new YamlPropertySourceLoader()
-    private Logger log = LoggerFactory.getLogger('grails.plugin.externalconfig.ExternalConfig')
+    private PropertiesPropertySourceLoader propertiesPropertySourceLoader = new PropertiesPropertySourceLoader()
+
     private String userHome = System.properties.getProperty('user.home')
     private String separator = System.properties.getProperty('file.separator')
 
@@ -47,10 +51,10 @@ class ExternalConfigRunListener implements SpringApplicationRunListener {
         String encoding = environment.getProperty('grails.config.encoding', String, 'UTF-8')
 
         for (location in locations) {
-            MapPropertySource propertySource = null
+            List<PropertySource<?>> propertySources = []
             Map currentProperties = getCurrentConfig(environment)
             if (location instanceof Class) {
-                propertySource = loadClassConfig(location as Class, currentProperties)
+                propertySources = loadClassConfig(location as Class, currentProperties)
             } else {
                 // Replace placeholders from known locations
                 String finalLocation = environment.resolvePlaceholders(location as String)
@@ -58,20 +62,20 @@ class ExternalConfigRunListener implements SpringApplicationRunListener {
                 Resource resource = defaultResourceLoader.getResource(finalLocation)
                 if (resource.exists()) {
                     if (finalLocation.endsWith('.groovy')) {
-                        propertySource = loadGroovyConfig(resource, encoding, currentProperties)
+                        propertySources = loadGroovyConfig(resource, encoding, currentProperties)
                     } else if (finalLocation.endsWith('.yml')) {
                         environment.activeProfiles
-                        propertySource = loadYamlConfig(resource)
+                        propertySources = loadYamlConfig(resource)
                     } else {
                         // Attempt to load the config as plain old properties file (POPF)
-                        propertySource = loadPropertiesConfig(resource)
+                        propertySources = loadPropertiesConfig(resource)
                     }
                 } else {
                     log.debug("Config file {} not found", [finalLocation] as Object[])
                 }
             }
-            if (propertySource?.getSource() && !propertySource.getSource().isEmpty()) {
-                environment.propertySources.addFirst(propertySource)
+            propertySources.each {
+                environment.propertySources.addFirst(it)
             }
         }
     }
@@ -128,7 +132,7 @@ class ExternalConfigRunListener implements SpringApplicationRunListener {
     }
 
     // Load groovy config from classpath
-    private MapPropertySource loadClassConfig(Class location, Map currentConfig) {
+    private static List<PropertySource<?>> loadClassConfig(Class location, Map currentConfig) {
         log.info("Loading config class {}", location.name)
         ConfigSlurper slurper = new ConfigSlurper(Environment.current.name)
         WriteFilteringMap filterMap = new WriteFilteringMap(currentConfig)
@@ -136,11 +140,11 @@ class ExternalConfigRunListener implements SpringApplicationRunListener {
         Map properties = slurper.parse((Class) location)?.flatten()
         Map writtenValues = filterMap.getWrittenValues()
         properties.putAll(writtenValues)
-        new MapPropertySource(location.toString(), properties)
+        return [new MapPropertySource(location.toString(), properties)] as List<PropertySource<?>>
     }
 
     // Load groovy config from resource
-    private MapPropertySource loadGroovyConfig(Resource resource, String encoding, Map currentConfig) {
+    private static List<PropertySource<?>> loadGroovyConfig(Resource resource, String encoding, Map currentConfig) {
         log.info("Loading groovy config file {}", resource.URI)
         String configText = resource.inputStream.getText(encoding)
         ConfigSlurper slurper = new ConfigSlurper(Environment.current.name)
@@ -150,39 +154,18 @@ class ExternalConfigRunListener implements SpringApplicationRunListener {
         Map<String, Object> properties = configText ? configObject?.flatten() as Map<String, Object> : [:]
         Map writtenValues = filterMap.getWrittenValues()
         properties.putAll(writtenValues)
-        new MapPropertySource(resource.filename, properties)
+        return [new MapPropertySource(resource.filename, properties)] as List<PropertySource<?>>
     }
 
-    private NavigableMapPropertySource loadYamlConfig(Resource resource) {
+    private List<PropertySource<?>> loadYamlConfig(Resource resource) {
         log.info("Loading YAML config file {}", resource.URI)
-        NavigableMapPropertySource propertySource = yamlPropertySourceLoader.load(resource.filename, resource, null)?.first() as NavigableMapPropertySource
-        return propertySource
+        return yamlPropertySourceLoader.load(resource.filename, resource, null)
     }
 
-    private MapPropertySource loadPropertiesConfig(Resource resource) {
+    private List<PropertySource<?>> loadPropertiesConfig(Resource resource) {
         log.info("Loading properties config file {}", resource.URI)
-        Properties properties = new Properties()
-        properties.load(resource.inputStream)
-        new MapPropertySource(resource.filename, properties as Map)
+        return propertiesPropertySourceLoader.load(resource.filename, resource)
     }
-
-    // Spring Boot 1.4 or higher
-    void starting() {}
-    // Spring Boot 1.3
-    void started() {}
-
-    void contextPrepared(ConfigurableApplicationContext context) {}
-
-    void contextLoaded(ConfigurableApplicationContext context) {}
-
-    // Spring Boot 2.1+
-    void started(ConfigurableApplicationContext context) {}
-
-    void running(ConfigurableApplicationContext context) {}
-
-    void failed(ConfigurableApplicationContext context, Throwable exception) {}
-
-    void finished(ConfigurableApplicationContext context, Throwable exception) {}
 
     static Map getCurrentConfig(ConfigurableEnvironment environment) {
         return new PropertySourcesConfig(environment.propertySources)
