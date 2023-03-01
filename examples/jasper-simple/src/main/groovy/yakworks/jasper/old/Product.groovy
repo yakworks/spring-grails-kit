044a33aa77082167136845c1304d0b20b8a8f2ee
Joshua B <joshdev@9ci.com>
2023-03-01 07:57:01 -0700

    remove old jasper-app

---
+++ examples/jasper-simple/src/main/groovy/yakworks/jasper/old/Product.groovy
@@ -0,0 +1,19 @@
+package yakworks.jasper.old
+
+import grails.compiler.GrailsCompileStatic
+import grails.persistence.Entity
+
+/**
+ * Created by basejump on 10/15/16.
+ */
+@Entity
+@GrailsCompileStatic
+class Product {
+    ProductGroup group
+    String num
+    String name
+
+    static constraints = {
+        num nullable:true
+    }
+}
